0c264e745162fa7624e27523ba1cfab93608e64d
sibas1 <117487764+sibas1@users.noreply.github.com>
2023-10-16 20:10:25 -0500

    estilos detallle

--- src/src/pages/DatailChapion.jsx
+++ src/src/pages/DatailChapion.jsx
@@ -16,13 +16,25 @@ function DatailChapion() {
     }
   }, [])
   console.log(detail)
-  return (<>
-    {(detail != undefined) ? (<div>
-      <p>{detail[champion].id}</p>
-      <img src={URLIm+champion+"_0.jpg"}></img>
-      {detail[champion].spells.map((x) => <><p>{x.name}</p><img src={URLSp + x.image.group +"/"+ x.image.full} alt={x.image.full} /></>)}
-      </div>):null}
-    </>
+  return (
+    <div>
+    {(detail != undefined) ? (
+        <div className='flex flex-col w-screen items-center'>
+          <section className='relative min-h-[75vh] pt-[30px] pb-[75px] w-screen bg-black'>
+            <div className='relative w-full pb-[50%]'>
+              <div className='absolute w-[110%] h-[110%]'>
+                <img className='blur object-cover object-center w-[100%] h-[100%]' src={URLIm + champion + "_0.jpg"}></img>
+              </div>
+            </div>
+            <h1 className='z-50 relative text-4xl text-center'>
+              <span ><div><span>{detail[champion].title}</span></div></span>
+              <span ><strong><div><span>{detail[champion].id}</span></div></strong></span>
+            </h1>
+          </section>
+          
+         {detail[champion].spells.map((x) => <><p>{x.name}</p><img className='z-50' src={URLSp + x.image.group + "/" + x.image.full} alt={x.image.full} /></>)}
+    </div>):null}
+  </div>
   )
 }
 
